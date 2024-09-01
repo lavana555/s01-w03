3a224dc0e806eac4e6a66189cdc495ddb55c480f
lavana55555 <nikita.timofeev@yumasoft.com>
2024-09-01 16:25:55 +0300

    added await

--- src/blogs/findBlogController.ts
+++ src/blogs/findBlogController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import {blogsRepository} from "../db/blog-repository";
 
 export const findBlogController = async (req: Request, res: Response) => {
-    const findValue = blogsRepository.find(req.params.id);
+    const findValue = await blogsRepository.find(req.params.id);
     if (findValue) {
         return res.status(200).json(findValue)
     } else {
