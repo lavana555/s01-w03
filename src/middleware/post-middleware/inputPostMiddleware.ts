844d353ebdc39cc1eb6cdff21c350eb23facb90f
lavana55555 <nikita.timofeev@yumasoft.com>
2024-09-01 16:58:11 +0300

    updated post, put error msg

--- src/middleware/post-middleware/inputPostMiddleware.ts
+++ src/middleware/post-middleware/inputPostMiddleware.ts
@@ -1,20 +1,31 @@
 import Joi from "joi";
 import {NextFunction, Request, Response} from "express";
+import {blogsRepository} from "../../db/blog-repository";
 
 export const inputPostMiddleware = (schema: Joi.ObjectSchema, source: 'body' | 'params' | 'query' = 'body') => {
     const resStatus = source === 'params' ? 404 : 400;
 
-    return (req: Request, res: Response, next: NextFunction) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
         const { title, shortDescription, content, blogId } = req.body;
         const validateParams = source === 'body' ? { title, shortDescription, content, blogId } : req[source];
         const { error } = schema.validate(validateParams, { abortEarly: false });
 
-        if (error) {
-            return res.status(resStatus).json({
-                errorsMessages: error.details.map(err => ({
-                    message: err.message || null,
-                    field: err.path[0] || null
-                }))
+        if(error) {
+            const validationErrors = error.details.map(err=>({
+                message:err.message || null,
+                field:err.path[0] || null,
+            }))
+            const findBlog = await blogsRepository.find(blogId)
+            console.log("findBlog", findBlog);
+            if (!findBlog) {
+                console.log("if");
+                validationErrors.push({
+                    message: 'Blog not found',
+                    field: 'blogId',
+                });
+            }
+            return res.status(400).json({
+                errorsMessages: validationErrors,
             });
         }
 
--- src/middleware/post-middleware/putPostMiddleware.ts
+++ src/middleware/post-middleware/putPostMiddleware.ts
@@ -1,17 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
+import {blogsRepository} from "../../db/blog-repository";
 
 export const putPostMiddleware = (schema: Joi.ObjectSchema, source: 'body' | 'params' | 'query' = 'body') => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const { error, value } = schema.validate(req[source], { abortEarly: false });
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const { title, shortDescription, content, blogId } = req.body;
+        const validateParams = source === 'body' ? { title, shortDescription, content, blogId } : req[source];
+        const { error, value } = schema.validate(validateParams, { abortEarly: false });
+
+        let errorsMessages:any = [];
 
         if (error) {
-            return res.status(400).json({
-                errorsMessages: error.details.map(err => ({
-                    message: err.message || null,
-                    field: err.context?.key || null
-                }))
-            });
+            errorsMessages = error.details.map(err => ({
+                message: err.message || null,
+                field: err.context?.key || null
+            }));
+        }
+
+        if (value.blogId) {
+            const findBlog = await blogsRepository.find(value.blogId);
+            if (!findBlog) {
+                errorsMessages.push({
+                    message: 'Blog not found',
+                    field: "blogId"
+                });
+            }
+        }
+
+        if (errorsMessages.length > 0) {
+            return res.status(400).json({ errorsMessages });
         }
        return next();
     };
